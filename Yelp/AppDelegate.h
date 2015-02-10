69258eaef87215ad72392f803d43aa0fce53e786
Calvin Tuong <ctuong@box.com>
2015-02-10 01:06:56 -0800

    Initial Commit

---
+++ Yelp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Yelp
+//
+//  Created by Calvin Tuong on 2/10/15.
+//  Copyright (c) 2015 Calvin Tuong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Yelp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Yelp
+//
+//  Created by Calvin Tuong on 2/10/15.
+//  Copyright (c) 2015 Calvin Tuong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
